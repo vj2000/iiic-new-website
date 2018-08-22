9e13300d050e2597a79b7177ef54354ad41ffe16
thesmallstar <42006277+thesmallstar@users.noreply.github.com>
2018-08-22 23:43:23 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -134,17 +134,18 @@
 
 			if ( scrlTop > 500 && scrlTop <= 2000 ) {
 				header.addClass('navbar-fixed-top fh5co-animated slideInDown');
-			} else if ( scrlTop <= 500) {
+			} else if ( scrlTop <= 500 && window.location != 'https://newiiic.herokuapp.com/contact.php') {
 				if ( header.hasClass('navbar-fixed-top') ) {
 					header.addClass('navbar-fixed-top fh5co-animated slideOutUp');
 					setTimeout(function(){
 						header.removeClass('navbar-fixed-top fh5co-animated slideInDown slideOutUp');
 					}, 100 );
 				}
-			} 
-			
+			}
+
 		});
 	};
+
 
 
 
